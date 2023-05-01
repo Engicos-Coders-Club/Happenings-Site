0c288cc8e539591e9f868cbc56e55661f7515d47
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-01 10:31:26 +0530

    .

--- Frontend/Site/src/App.js
+++ Frontend/Site/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Events from './components/Events'
 import Modal from './components/Modal'
+import Sponsor from './components/Sponsor'
+import Schedule from './components/Schedule'
 function App() {
   return (
     <>
-    {/* <Events/> */}
-    <Modal/>
+    <Events/>
+    {/* <Modal/> */}
+    {/* <Sponsor/> */}
+    {/* <Schedule/> */}
     </>
   );
 }
