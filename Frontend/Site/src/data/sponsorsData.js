36f662718830b7305c2e732ee712d9802b44cf1c
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-01 20:17:44 +0530

    Sponsor Section

---
+++ Frontend/Site/src/data/sponsorsData.js
@@ -0,0 +1,49 @@
+import Logo from '../assets/Logo.png'
+
+const sponsors=[
+    {
+        title:"TITLE SPONSORS",
+        collection:[
+            {
+                img: Logo,
+                link: ''
+            },
+            {
+                img: Logo,
+                link: ''
+            },
+            {
+                img: Logo,
+                link: ''
+            }
+        ]
+    },
+    {
+        title:"CO SPONSORS",
+        collection:[
+            {
+                img: Logo,
+                link: ''
+            },
+            {
+                img: Logo,
+                link: ''
+            }
+        ]
+    },
+    {
+        title:"ASSOCIATIVE SPONSORS",
+        collection:[
+            {
+                img: Logo,
+                link: ''
+            },
+            {
+                img: Logo,
+                link: ''
+            }
+        ]
+    },
+]
+
+export {sponsors}
