a9a9a90430111fafe6be2ad25328d2c816a4ede4
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-01 11:44:43 +0530

    Data added for event tab

---
+++ Frontend/Site/src/data/EventTabData.js
@@ -0,0 +1,31 @@
+import landscape from '../Landscape-Color.jpg'
+
+const eventsData =[
+    {
+        'img': landscape,
+        'link': '',
+        'eventName': 'E-Sport'
+    },
+    {
+        'img': landscape,
+        'link': '',
+        'eventName': 'Event'
+    },
+    {
+        'img': landscape,
+        'link': '',
+        'eventName': 'Event'
+    },
+    {
+        'img': landscape,
+        'link': '',
+        'eventName': 'Event'
+    },
+    {
+        'img': landscape,
+        'link': '',
+        'eventName': 'Event'
+    },
+]
+
+export {eventsData};
