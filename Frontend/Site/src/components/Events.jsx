019aac84d80adb0a8a2a98a5b427a4ac481cec01
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-01 11:45:14 +0530

    Event section updated

--- Frontend/Site/src/components/Events.jsx
+++ Frontend/Site/src/components/Events.jsx
@@ -8,10 +8,10 @@ import {
 import { gsap } from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect } from 'react';
-import landscape from '../Landscape-Color.jpg'
+import EventTab from './EventTab';
+import { eventsData } from '../data/EventTabData'
 
 function Events() {
-  const clip={'clip-path': 'polygon(10% 0%, 90% 0%, 100% 20%, 100% 80%, 90% 100%, 10% 100%, 0% 80%, 0% 20%)'}
 
   gsap.registerPlugin(ScrollTrigger);
 
@@ -127,87 +127,18 @@ function Events() {
           {/* Slider Area */}
           <div id="Slider" className='box-content flex w-[300%] h-3/4'>
 
-            {/* Event Tab */}
-            <div  className="min-w-[27%] xs:min-w-[24%] sm:min-w-[11%] md:min-w-[14%] lg:min-w-[18%] h-3/4 m-4 md:m-10">
-              
-              {/* Image */}
-              <div className='w-full h-full' style={clip}>
-                <img src={landscape} alt=""  style={{'transition':'transform 0.8s'}} className='bg-cover hover:scale-125' />
-              </div>
-
-              {/* Text Button */}
-              <div className='flex justify-between mt-10'>
-                <p className="text-white text-3xl md:text-4xl lg:text-5xl" style={{'fontFamily':'MangoGrotesque'}}>EVENTS</p>
-                <button className='h-1/2 bg-orange-600 px-2 md:px-5 py-2 rounded-md hover:bg-orange-700 hover:scale-105 uppercase mb-6 md:mb-0 text-[12px] md:text-sm'>View all</button>
-              </div>
-            </div>
-
-            {/* Event Tab */}
-            <div  className="min-w-[27%] xs:min-w-[24%] sm:min-w-[11%] md:min-w-[14%] lg:min-w-[18%] h-3/4 m-4 md:m-10">
-              
-              {/* Image */}
-              <div className='w-full h-full' style={clip}>
-                <img src={landscape} alt=""  style={{'transition':'transform 0.8s'}} className='bg-cover hover:scale-125' />
-              </div>
-
-              {/* Text Button */}
-              <div className='flex justify-between mt-10'>
-                <p className="text-white text-3xl md:text-4xl lg:text-5xl" style={{'fontFamily':'MangoGrotesque'}}>EVENTS</p>
-                <button className='h-1/2 bg-orange-600 px-2 md:px-5 py-2 rounded-md hover:bg-orange-700 hover:scale-105 uppercase mb-6 md:mb-0 text-[12px] md:text-sm'>View all</button>
-              </div>
-            </div>
-
-            {/* Event Tab */}
-            <div  className="min-w-[27%] xs:min-w-[24%] sm:min-w-[11%] md:min-w-[14%] lg:min-w-[18%] h-3/4 m-4 md:m-10">
-              
-              {/* Image */}
-              <div className='w-full h-full' style={clip}>
-                <img src={landscape} alt=""  style={{'transition':'transform 0.8s'}} className='bg-cover hover:scale-125' />
-              </div>
-
-              {/* Text Button */}
-              <div className='flex justify-between mt-10'>
-                <p className="text-white text-3xl md:text-4xl lg:text-5xl" style={{'fontFamily':'MangoGrotesque'}}>EVENTS</p>
-                <button className='h-1/2 bg-orange-600 px-2 md:px-5 py-2 rounded-md hover:bg-orange-700 hover:scale-105 uppercase mb-6 md:mb-0 text-[12px] md:text-sm'>View all</button>
-              </div>
-            </div>
-
-            {/* Event Tab */}
-            <div  className="min-w-[27%] xs:min-w-[24%] sm:min-w-[11%] md:min-w-[14%] lg:min-w-[18%] h-3/4 m-4 md:m-10">
-              
-              {/* Image */}
-              <div className='w-full h-full' style={clip}>
-                <img src={landscape} alt=""  style={{'transition':'transform 0.8s'}} className='bg-cover hover:scale-125' />
-              </div>
-
-              {/* Text Button */}
-              <div className='flex justify-between mt-10'>
-                <p className="text-white text-3xl md:text-4xl lg:text-5xl" style={{'fontFamily':'MangoGrotesque'}}>EVENTS</p>
-                <button className='h-1/2 bg-orange-600 px-2 md:px-5 py-2 rounded-md hover:bg-orange-700 hover:scale-105 uppercase mb-6 md:mb-0 text-[12px] md:text-sm'>View all</button>
-              </div>
-            </div>
-
-            {/* Event Tab */}
-            <div  className="min-w-[27%] xs:min-w-[24%] sm:min-w-[11%] md:min-w-[14%] lg:min-w-[18%] h-3/4 m-4 md:m-10">
-              
-              {/* Image */}
-              <div className='w-full h-full' style={clip}>
-                <img src={landscape} alt=""  style={{'transition':'transform 0.8s'}} className='bg-cover hover:scale-125' />
-              </div>
-
-              {/* Text Button */}
-              <div className='flex justify-between mt-10'>
-                <p className="text-white text-3xl md:text-4xl lg:text-5xl" style={{'fontFamily':'MangoGrotesque'}}>EVENTS</p>
-                <button className='h-1/2 bg-orange-600 px-2 md:px-5 py-2 rounded-md hover:bg-orange-700 hover:scale-105 uppercase mb-6 md:mb-0 text-[12px] md:text-sm'>View all</button>
-              </div>
-            </div>
+            {
+              eventsData.map((event)=>{
+                  return(
+                    <EventTab img={event.img} link={event.link} eventName={event.eventName}/>
+                  )
+              })
+            }
+            
+            
 
             
-           
-
-
-
-
+        
             
           </div>
               
