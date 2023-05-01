ca8d7a8df2da555faf766986a087e1b4dee0c92f
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-01 10:30:38 +0530

    Media query added

--- Frontend/Site/tailwind.config.js
+++ Frontend/Site/tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '450px',
+      
+      'sm': '576px',
+
+      'md': '960px',
+
+      'lg': '1440px',
+    },
     extend: {},
   },
   plugins: [],
