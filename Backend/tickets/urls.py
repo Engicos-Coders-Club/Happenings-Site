138bfb64a709b33f799614660e64872b005d388f
Atharva Parkhe <patharv777@gmail.com>
2023-05-03 12:23:03 +0530

    addind data

--- Backend/tickets/urls.py
+++ Backend/tickets/urls.py
@@ -5,10 +5,10 @@ from .views import *
 
 urlpatterns = [
 
-    path('all-tickets/', views.GetTickets.as_view(), name="all-tickets"),
+    # path('all-tickets/', views.GetTickets.as_view(), name="all-tickets"),
 
-    path('buy-ticket/<ticket_id>/', views.buy_tickets, name="buy-ticket"),
+    # path('buy-ticket/<ticket_id>/', views.buy_tickets, name="buy-ticket"),
     
-    path('buy-ticket-result/<ticket_id>/', views.buy_tickets, name="buy-ticket"),
+    # path('buy-ticket-result/<ticket_id>/', views.buy_tickets, name="buy-ticket"),
 
 ]
