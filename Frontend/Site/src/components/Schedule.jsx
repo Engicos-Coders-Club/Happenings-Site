ab1a8c916cb3fe5b9302f728f54ad7e5b2b06861
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-07 00:18:21 +0530

    minor change

--- Frontend/Site/src/components/Schedule.jsx
+++ Frontend/Site/src/components/Schedule.jsx
@@ -76,7 +76,7 @@ function Schedule(props) {
 
 
   return (
-    <section id="schedule" className='bg-[#171717ff] schedule opacity-0 h-screen relative md:pl-16  mt-[8400px] sm:mt-[5400px] md:mt-[3700px] lg:mt-[2550px]' style={{'fontFamily':'MangoGrotesque'}}>
+    <section id="schedule" className='bg-[#171717ff] schedule opacity-0 h-auto relative md:pl-16  mt-[8400px] sm:mt-[5400px] md:mt-[3700px] lg:mt-[2550px]' style={{'fontFamily':'MangoGrotesque'}}>
 {/* , 'display':'grid','gridTemplateRows':'22% 55% 25%' */}
       <Navbar />
       <SideBar />
