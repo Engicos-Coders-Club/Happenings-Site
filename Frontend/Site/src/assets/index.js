300b93e3176c47a79981b73de8660cfbd098e089
Greeshma Medam <greeshma.medam@gmail.com>
2023-05-16 08:34:53 +0530

    Update assets index

--- Frontend/Site/src/assets/index.js
+++ Frontend/Site/src/assets/index.js
@@ -1,9 +1,6 @@
 import eventsBg from './events-bg.webp'
 import eventsCard from './events-card.webp'
 import modalBg from './modalBg.webp'
-import ticket1 from './ticket-1.svg'
-import ticket2 from './ticket-2.svg'
-import ticket3 from './ticket-3.svg'
 import footerleft from './footer-left.png'
 import footerright from './footer-right.png'
 
@@ -11,9 +8,6 @@ export {
     eventsBg,
     eventsCard,
     modalBg,
-    ticket1,
-    ticket2,
-    ticket3,
     footerleft,
     footerright
 }
