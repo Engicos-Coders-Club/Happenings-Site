f41b918c0c0b1e266d2992ca8e8bd5be83d05967
Greeshma Medam <greeshma.medam@gmail.com>
2023-05-16 10:14:53 +0530

    Fix team images

---
+++ Frontend/Site/src/assets/teams/council/index.js
@@ -0,0 +1,29 @@
+import akash from './akash.jpg';
+import atharva from './atharva.jpg';
+import divya from './divya.jpg';
+import mansi from './mansi.jpg';
+import nihal from './nihal.jpg';
+import ruth from './ruth.jpg';
+import saara from './saara.jpg';
+import saiel from './saiel.jpg';
+import shazz from './shazz.jpg';
+import sumit from './sumit.jpg';
+import swapnil from './swapnil.jpg';
+import urvi from './urvi.jpg';
+import sarthak from './sarthak.jpg';
+
+export {
+    akash,
+    atharva,
+    divya,
+    mansi,
+    nihal,
+    ruth,
+    saara,
+    saiel,
+    shazz,
+    sumit,
+    swapnil,
+    urvi,
+    sarthak
+}
---
+++ Frontend/Site/src/data/Teams_Info/Council.js
@@ -0,0 +1,94 @@
+import akash from '../../assets/teams/council/akash.jpg';
+import atharva from '../../assets/teams/council/atharva.jpg';
+import divya from '../../assets/teams/council/divya.jpg';
+import mansi from '../../assets/teams/council/mansi.jpg';
+import nihal from '../../assets/teams/council/nihal.jpg';
+import ruth from '../../assets/teams/council/ruth.jpg';
+import saara from '../../assets/teams/council/saara.jpg';
+import saiel from '../../assets/teams/council/saiel.jpg';
+import shazz from '../../assets/teams/council/shazz.jpg';
+import sumit from '../../assets/teams/council/sumit.jpg';
+import swapnil from '../../assets/teams/council/swapnil.jpg';
+import urvi from '../../assets/teams/council/urvi.jpg';
+import sarthak from '../../assets/teams/council/sarthak.jpg';
+
+export const councilData = [
+  {
+    Name: "Sumit Komarpant",
+    Designation: "GENERAL SECRETARY",
+    Contact: "8999579536",
+    pic: sumit
+  },
+  {
+    Name: "Ruthveek Dessai",
+    Designation: "CHAIRMAN",
+    Contact: "9309981749",
+    pic: ruth
+  },
+  {
+    Name: "Sarthak Bandodkar",
+    Designation: "CULTURAL SECRETARY",
+    Contact: "7057496089",
+    pic: sarthak
+  },
+  {
+    Name: "Shazeab Sayed",
+    Designation: "PUBLIC RELATION SECRETARY",
+    Contact: "7028889127",
+    pic: shazz
+  },
+  {
+    Name: "Nehal Sinari",
+    Designation: "ACADEMIC SECRETARY",
+    Contact: "9689778470",
+    pic: nihal
+  },
+  {
+    Name: "Akash Gauns",
+    Designation: "GYMKHANA SECRETARY",
+    Contact: "7768804271",
+    pic: akash
+  },
+  {
+    Name: "Atharva Parkhe",
+    Designation: "PLACEMENT SECRETARY",
+    Contact: "8007609672",
+    pic: atharva
+  },
+  {
+    Name: "Mansi Mishra",
+    Designation: "MAGAZINE SECRETARY",
+    Contact: "8459501297",
+    pic: mansi
+  },
+  {
+    Name: "Divya Ramnathker",
+    Designation: "LADIES SECRETARY",
+    Contact: "9371919234",
+    pic: divya
+  },
+  {
+    Name: "Swapnil gawas",
+    Designation: "AUDITOR 1",
+    Contact: "9762233006",
+    pic: swapnil
+  },
+  {
+    Name: "Saiel Pai Kane",
+    Designation: "AUDITOR 2",
+    Contact: "7038788388",
+    pic: saiel
+  },
+ {
+    Name: "Saara Sehgal",
+    Designation: "LADIES REPRESENTATIVE 1",
+    Contact: "9561092581",
+    pic: saara
+  },
+  {
+    Name: "Urvi Gaundalkar",
+    Designation: "LADIES REPRESENTATIVE 2",
+    Contact: "7057942384",
+    pic: urvi
+  }
+]
