120b00264803c1f86740c89b16ed1786224d5f57
puneetmpatil <patilpuneet01@gmail.com>
2023-05-17 14:50:00 +0530

    Sponsors section added

--- Frontend/Site/src/data/sponsorsData.js
+++ Frontend/Site/src/data/sponsorsData.js
@@ -1,46 +1,97 @@
-import Logo from '../assets/Logo.png'
+import etherentExpress from '../../public/assets/sponsors/internet/ethernetExpress.png' 
+import AnandBose from '../../public/assets/sponsors/eventsponsors/anandbose.webp'
+import saurabhlotlikargroup from '../../public/assets/sponsors/eventsponsors/saurabhlotlikargroup.webp'
+import ambika from '../../public/assets/sponsors/supporters/Ambika.webp'
+import deccan from '../../public/assets/sponsors/supporters/deccan.webp'
+import himalaya from '../../public/assets/sponsors/supporters/himalaya.webp'
+import mycanadavisas from '../../public/assets/sponsors/supporters/mycanadavisas.webp'
+import sanaointernational from '../../public/assets/sponsors/supporters/sanaointernational.webp'
+import fcgoa from '../../public/assets/sponsors/sports/FCGOa.png'
+import sbi from '../../public/assets/sponsors/eventsponsors/SBI.webp'
+import casant from '../../public/assets/sponsors/title/Casant.webp'
+import jvshetty from '../../public/assets/sponsors/supporters/JVShetty.webp'
+import unionBank from '../../public/assets/sponsors/supporters/unionBank.webp'
+import blueskript from '../../public/assets/sponsors/supporters/blueskript.webp'
 
 const sponsors=[
     {
         title:"TITLE SPONSORS",
         collection:[
             {
-                img: Logo,
-                link: ''
+                img: casant,
+                link: 'https://casant.in/'
             },
-            {
-                img: Logo,
-                link: ''
-            },
-            {
-                img: Logo,
-                link: ''
-            }
         ]
     },
     {
-        title:"CO SPONSORS",
+        title:"SPORTS SPONSORS",
         collection:[
             {
-                img: Logo,
-                link: ''
+                img: fcgoa,
+                link: 'https://fcgoa.in/'
             },
-            {
-                img: Logo,
-                link: ''
-            }
         ]
     },
     {
-        title:"ASSOCIATIVE SPONSORS",
+        title:"INTERNET SPONSORS",
         collection:[
             {
-                img: Logo,
+                img: etherentExpress,
+                link: 'https://www.expl.in/'
+            },
+        ]
+    },
+    {
+        title:"EVENT SPONSORS",
+        collection:[
+            {
+                img: AnandBose,
                 link: ''
             },
             {
-                img: Logo,
+                img: saurabhlotlikargroup,
                 link: ''
+            },
+            {
+                img: sbi,
+                link: 'https://www.onlinesbi.sbi/'
+            },
+        ]
+    },
+    {
+        title:"SUPPORTERS",
+        collection:[
+            {
+                img: sanaointernational,
+                link: 'https://www.sanaointl.com/'
+            },
+            {
+                img: mycanadavisas,
+                link: 'https://mycanadavisas.com/'
+            },
+            {
+                img: deccan,
+                link: 'https://www.deccanchemicals.com/'
+            },
+            {
+                img: ambika,
+                link: ''
+            },
+            {
+                img: himalaya,
+                link: 'https://himalayawellness.in/'
+            },
+            {
+                img: jvshetty,
+                link: ''
+            },
+            {
+                img: unionBank,
+                link: 'https://www.unionbankofindia.co.in/english/home.aspx'
+            },
+            {
+                img: blueskript,
+                link: 'https://blueskript.com/'
             }
         ]
     },
