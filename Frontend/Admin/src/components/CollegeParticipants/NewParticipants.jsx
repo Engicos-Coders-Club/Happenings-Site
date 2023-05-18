d9986942432051bb5523b9bb15ce552423f79c35
Greeshma Medam <greeshma.medam@gmail.com>
2023-05-18 18:35:34 +0530

    Load data for events

--- Frontend/Admin/src/components/CollegeParticipants/NewParticipants.jsx
+++ Frontend/Admin/src/components/CollegeParticipants/NewParticipants.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react';
-import { Typography, TextField, Select, MenuItem, FormControl, InputLabel,Backdrop,CircularProgress,Tabs,Tab } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { useNavigate } from 'react-router-dom';
-import { Container } from '@mui/material';
-import { ArrowBack as ArrowBackIcon, Search as SearchIcon } from '@material-ui/icons';
-import ParticipantCard from './NewParticipantCard';
-import img from '../../assets/happenings-logo.png'
-import { useState } from 'react';
-import {getAllColleges,getAllCollegeParticipants,getDay1CollegeParticipants,getDay2CollegeParticipants} from '../../store/actions/college'
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import {
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Backdrop,
+  CircularProgress,
+  Tabs,
+  Tab,
+  Box,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useNavigate } from "react-router-dom";
+import { Container } from "@mui/material";
+import {
+  ArrowBack as ArrowBackIcon,
+  Search as SearchIcon,
+} from "@material-ui/icons";
+import ParticipantCard from "./NewParticipantCard";
+import img from "../../assets/happenings-logo.png";
+import { useState } from "react";
+import {
+  getAllColleges,
+  getAllCollegeParticipants,
+  getDay1CollegeParticipants,
+  getDay2CollegeParticipants,
+} from "../../store/actions/college";
+import { useDispatch, useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -49,6 +69,7 @@ const useStyles = makeStyles((theme) => ({
     gap: "1rem",
     justifyContent: "flex-start",
     margin: "1rem 0",
+    flexWrap: "wrap",
   },
   btnTab: {
     border: "1px dashed grey",
@@ -61,20 +82,22 @@ const useStyles = makeStyles((theme) => ({
 
 const Participants = () => {
   const classes = useStyles();
-  const dispatch = useDispatch()
-  const [searchValue, setSearchValue] = useState('');
-  const [value,setValue] = useState('all')
-  const [cid,setCid] = useState(null)
+  const dispatch = useDispatch();
+  const [searchValue, setSearchValue] = useState("");
+  const [value, setValue] = useState("all");
+  const [cid, setCid] = useState(null);
 
-  useEffect(()=>{
-    initialize()
-  },[])
+  useEffect(() => {
+    initialize();
+  }, []);
 
-  const initialize = async() =>{
-    await dispatch(getAllColleges())
-  }
+  const initialize = async () => {
+    await dispatch(getAllColleges());
+  };
 
-  const {colleges,loading,participants} = useSelector((state)=>state.college)
+  const { colleges, loading, loadingCollege, participants } = useSelector(
+    (state) => state.college
+  );
 
   // const handleSearchChange = (e) => {
   //   setSearchValue(e.target.value);
@@ -82,13 +105,10 @@ const Participants = () => {
 
   const handleChange = (event, newValue) => {
     //console.log('Selected tab value:', newValue);
-    setValue(newValue)
-    if(newValue == 'day1')
-      dispatch(getDay1CollegeParticipants(cid))
-    else if (newValue == 'day2')
-      dispatch(getDay2CollegeParticipants(cid))
-    else
-      dispatch(getAllCollegeParticipants(cid))
+    setValue(newValue);
+    if (newValue == "day1") dispatch(getDay1CollegeParticipants(cid));
+    else if (newValue == "day2") dispatch(getDay2CollegeParticipants(cid));
+    else dispatch(getAllCollegeParticipants(cid));
   };
 
   const [member, setMember] = useState([
@@ -124,120 +144,145 @@ const Participants = () => {
     });
   };
 
-  const handleCollege = (e) =>{
-    dispatch(getAllCollegeParticipants(e.target.value))
-    setCid(e.target.value)
-  }
+  const handleCollege = (e) => {
+    dispatch(getAllCollegeParticipants(e.target.value));
+    setCid(e.target.value);
+  };
 
-  if(loading || !colleges)
-    return ( 
-    <Backdrop open={true}>
-      <CircularProgress/>
-    </Backdrop>
-  )
+  if (loading || !colleges)
+    return (
+      <Backdrop open={true}>
+        <CircularProgress />
+      </Backdrop>
+    );
   else
     return (
-    <Container maxWidth="xl">      
-      <div className={classes.header}>
-        <Typography variant="h4" className={classes.headTitle}>
-          College PARTICIPANTS
-        </Typography>
-      </div>
-    
-      {/* <TextField
-          className={classes.searchBar}
-          variant="outlined"
-          placeholder="Search"
-          InputProps={{
-            endAdornment: <SearchIcon />,
-          }}
-          value={searchValue}
-          onChange={handleSearchChange}
-        /> */}
-        
-      <div className={classes.inputContainer}>
-        <Typography variant="h5" className={classes.eventHeader}>
-          SELECT COLLEGE&nbsp;
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.4993 0.833338L32.166 16.5L16.4993 32.1667L13.7087 29.425L24.6754 18.4583L0.832682 18.4583L0.832682 14.5417L24.6754 14.5417L13.7087 3.575L16.4993 0.833338Z"
-              fill="black"
-            />
-          </svg>
-        </Typography>
+      <Container maxWidth="xl">
+        <div className={classes.header}>
+          <Typography variant="h4" className={classes.headTitle}>
+            College PARTICIPANTS
+          </Typography>
+        </div>
 
-        <FormControl
-          variant="outlined"
-          fullWidth
-          className={classes.selectContainer}
-        >
-          <InputLabel id="college-label">Select College</InputLabel>
-          <Select labelId="college-label" label="Select College" onChange={handleCollege}>
-            <MenuItem value="" disabled>Select College</MenuItem>
-            {
-              colleges.map((ele)=>
-                <MenuItem key={ele.id} value={ele.id}>{ele.college_name}</MenuItem>
-              )
-            }
-          </Select>
-        </FormControl>
-      </div>
-
-      <div className={classes.eventHeader}>
-        <Typography variant="h5" className={classes.eventHeader}>
-          VIEW EVENT PARTICIPANTS&nbsp;
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.4993 0.833338L32.166 16.5L16.4993 32.1667L13.7087 29.425L24.6754 18.4583L0.832682 18.4583L0.832682 14.5417L24.6754 14.5417L13.7087 3.575L16.4993 0.833338Z"
-              fill="black"
-            />
-          </svg>
-        </Typography>
-      </div>
-
-      <Tabs
-        className={classes.btnGrp}
-        value={value}
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={handleChange}
-      >
-        <Tab
-          variant="contained"
-          className={classes.btnTab}
-          label="All days"
-          value='all'
-        ></Tab>
-        <Tab variant="contained" value='day1' className={classes.btnTab} label="Day 1"></Tab>
-        <Tab variant="contained" value='day2' className={classes.btnTab} label="Day 2"></Tab>
-      </Tabs>
-
-      <div className={classes.eventContent}>
-        {participants && participants.map(
-          (member) =>
-            member.name.toLowerCase().includes(searchValue.toLowerCase()) && (
-              <ParticipantCard
-                key={member.id}
-                member={member}
-                handleCheckboxToggle={handleCheckboxToggle}
+        <div className={classes.inputContainer}>
+          <Typography variant="h5" className={classes.eventHeader}>
+            SELECT COLLEGE&nbsp;
+            <svg
+              width="33"
+              height="33"
+              viewBox="0 0 33 33"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.4993 0.833338L32.166 16.5L16.4993 32.1667L13.7087 29.425L24.6754 18.4583L0.832682 18.4583L0.832682 14.5417L24.6754 14.5417L13.7087 3.575L16.4993 0.833338Z"
+                fill="black"
               />
-            )
-        )}
-      </div>
-    </Container>
-  );
+            </svg>
+          </Typography>
+
+          <FormControl
+            variant="outlined"
+            fullWidth
+            className={classes.selectContainer}
+          >
+            <InputLabel id="college-label">Select College</InputLabel>
+            <Select
+              labelId="college-label"
+              label="Select College"
+              onChange={handleCollege}
+            >
+              <MenuItem value="" disabled>
+                Select College
+              </MenuItem>
+              {colleges.map((ele) => (
+                <MenuItem key={ele.id} value={ele.id}>
+                  {ele.college_name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className={classes.eventHeader}>
+          <Typography variant="h5" className={classes.eventHeader}>
+            VIEW EVENT PARTICIPANTS&nbsp;
+            <svg
+              width="33"
+              height="33"
+              viewBox="0 0 33 33"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.4993 0.833338L32.166 16.5L16.4993 32.1667L13.7087 29.425L24.6754 18.4583L0.832682 18.4583L0.832682 14.5417L24.6754 14.5417L13.7087 3.575L16.4993 0.833338Z"
+                fill="black"
+              />
+            </svg>
+          </Typography>
+        </div>
+
+        <Tabs
+          className={classes.btnGrp}
+          value={value}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={handleChange}
+        >
+          <Tab
+            variant="contained"
+            className={classes.btnTab}
+            label="All days"
+            value="all"
+          ></Tab>
+          <Tab
+            variant="contained"
+            value="day1"
+            className={classes.btnTab}
+            label="Day 1"
+          ></Tab>
+          <Tab
+            variant="contained"
+            value="day2"
+            className={classes.btnTab}
+            label="Day 2"
+          ></Tab>
+        </Tabs>
+
+        <div className={classes.eventContent}>
+          {loadingCollege ? (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "200px",
+                bgcolor: "#F8F6F4",
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          ) : (
+            <>
+              {participants &&
+                participants.map(
+                  (member) =>
+                    member.name
+                      .toLowerCase()
+                      .includes(searchValue.toLowerCase()) && (
+                      <ParticipantCard
+                        key={member.id}
+                        member={member}
+                        handleCheckboxToggle={handleCheckboxToggle}
+                      />
+                    )
+                )}
+            </>
+          )}
+        </div>
+      </Container>
+    );
 };
 
 export default Participants;
